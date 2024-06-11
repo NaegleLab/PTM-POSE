6a97ae2ece034d7e38d6ff663caeac0ea788c0f9
srcrowl <sc8wf@virginia.edu>
2024-06-11 10:11:00 -0400

    Fixed scipy error in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Naegle Lab",
     author_email="kmn4mj@virginia.edu",
     url="https://github.com/NaegleLab/PTM-POSE/tree/main",
-    install_requires=['pandas==2.2.*', 'numpy==1.26.*', 'scipy=1.13.*', 'biopython==1.83.*', 'tqdm==4.66.*', 'xlrd', 'matplotlib', 'requests'],
+    install_requires=['pandas==2.2.*', 'numpy==1.26.*', 'scipy==1.13.*', 'biopython==1.83.*', 'tqdm==4.66.*', 'xlrd', 'matplotlib', 'requests'],
     license='GNU General Public License v3',
     description='PTM-POSE: PTM Projection onto Splice Events',
     long_description="""PTM-POSE is an open-source tool for annotating splice event quantification data with post-translational modifications (PTMs) and their functional consequences.""",
